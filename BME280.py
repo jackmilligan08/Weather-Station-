8f3c900788935d5cf3b73978da738e0dae92ed4f
jackmilligan08 <124310811+jackmilligan08@users.noreply.github.com>
2024-02-27 13:13:35 -0800

    Update BME280.py
    comments

--- BME280.py
+++ BME280.py
@@ -1,4 +1,4 @@
-import bme280
+#Libraries 
 import smbus2
 import time
 from time import sleep 
@@ -8,16 +8,16 @@ address = 0x77 # Adafruit BME280 address. Other BME280s may be different
 bus = smbus2.SMBus(port)
 
 bme280.load_calibration_params(bus,address)
-
-while True: 
+#Continuous Loop
+while True:
+#Recieving Data from BME280 
     bme280_data = bme280.sample(bus,address)
     humidity  = bme280_data.humidity
     pressure  = bme280_data.pressure
     ambient_temperature = bme280_data.temperature
+#Printing Data from BME280)
     print("Humidity:", humidity,"%")
     print("Pressure:", pressure,"hPa")
     print("Ambient_Temperature:", ambient_temperature,"*C")
     print("")
-
-
     sleep(1)
